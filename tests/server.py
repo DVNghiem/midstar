39b7f439dd7fb5fdb2ca60d241ead7764b911405
Dang Van Nghiem <vannghiem848@gmail.com>
2025-04-15 22:53:30 +0700

    Add CSRF and JWT middleware; update security headers middleware and configuration

--- tests/server.py
+++ tests/server.py
@@ -4,9 +4,10 @@ from starlette.applications import Starlette
 from midstar.middleware import (
     RateLimitMiddleware,
     EdgeCacheMiddleware,
-    SecurityMiddleware,
     ConcurrentRequestMiddleware,
-    CacheConfig
+    CacheConfig,
+    SecurityHeadersConfig,
+    SecurityHeadersMiddleware,
 )
 from midstar.core.backend import InMemoryBackend
 from starlette.middleware import Middleware
@@ -29,25 +30,16 @@ app = Starlette(
             EdgeCacheMiddleware,
             cache_config = CacheConfig(max_age=60)    
         ),
-        # Middleware(
-        #     SecurityMiddleware,
-        #     rate_limiting=True,
-        #     jwt_auth=True,
-        #     cors_configuration={
-        #         "allowed_origins": ["*"],
-        #         "allowed_methods": ["GET", "POST", "PUT", "DELETE"],
-        #         "max_age": 3600,
-        #     },
-        #     csrf_protection=True,
-        #     security_headers={
-        #         "X-Frame-Options": "DENY",
-        #         "X-Content-Type-Options": "nosniff",
-        #         "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
-        #     },
-        #     jwt_secret="your_jwt_secret",
-        #     jwt_algorithm="HS256",
-        #     jwt_expires_in=3600,
-        # ),
+        Middleware(
+            SecurityHeadersMiddleware,
+            config=SecurityHeadersConfig(
+                headers={
+                    "X-Content-Type-Options": "nosniff",
+                    "X-Frame-Options": "DENY",
+                    "Content-Security-Policy": "default-src 'self'",
+                }
+            ),
+        ),
     ]
 )
 
